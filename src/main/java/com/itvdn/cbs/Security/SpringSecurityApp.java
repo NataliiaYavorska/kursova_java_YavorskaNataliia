8bc902682ed7c8d05b9071b2290e28c9774a7e15
macbookpro <inform5817@gmial.com>
2024-08-15 11:27:13 +0300

    update

--- src/main/java/com/itvdn/cbs/Security/SpringSecurityApp.java
+++ src/main/java/com/itvdn/cbs/Security/SpringSecurityApp.java
@@ -7,20 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringSecurityApp {
-
-	private static PasswordEncoder passwordEncoder;
-
-	public SpringSecurityApp(PasswordEncoder passwordEncoder) {
-		this.passwordEncoder = passwordEncoder;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApp.class, args);
-
-		generatePass();
-	}
-
-	public static void generatePass() {
-		System.out.println("Encoded password: " + passwordEncoder.encode("12345"));
 	}
 }
